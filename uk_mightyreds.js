354fdd557fa89e71755f885e5af30afaa5d791e7
tbsetyanto <tb.setyanto@gmail.com>
2013-06-15 11:51:56 +0700

    Update uk_mightyreds.js

--- uk_mightyreds.js
+++ uk_mightyreds.js
@@ -4,3 +4,34 @@ function mociNdopok(lengko,tahutempe) {
 
 }
 
+var infolinks_pid = 1722223;
+var infolinks_wsid = 0;
+
+
+var incFile = "http://resources.infolinks.com/js/infolinks_main.js";
+document.write('<script type="text/javascript" src="'
+    + incFile+ '"></scr' + 'ipt>');
+   
+   
+   
+   
+   
+   
+   
+
+ch_client = "laviva";
+ch_width = 550;
+ch_height = 250;
+ch_type = "mpu";
+ch_sid = "Chitika Default";
+ch_color_site_link = "0000CC";
+ch_color_title = "0000CC";
+ch_color_border = "FFFFFF";
+ch_color_text = "000000";
+ch_color_bg = "FFFFFF";
+
+var inerQuotient = "http://scripts.chitika.net/eminimalls/amm.js";
+document.write('<script type="text/javascript" src="'
+    + inerQuotient+ '"></scr' + 'ipt>');
+
+
