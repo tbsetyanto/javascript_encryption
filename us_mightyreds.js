510ed906666534b9f5c505df0cb7b2e649c39975
tbsetyanto <tb.setyanto@gmail.com>
2013-09-22 17:02:37 +0700

    Update us_mightyreds.js

--- us_mightyreds.js
+++ us_mightyreds.js
@@ -1,6 +1,6 @@
 function mociNdopok(lengko,tahutempe) {
 
-  return document.location.href="http://www.amazon.com/gp/product/"+lengko+"?tag=smartshopp063-20";
+  return document.location.href="http://www.amazon.com/gp/product/"+lengko+"?tag=davearwil-20";
 
 }
 
