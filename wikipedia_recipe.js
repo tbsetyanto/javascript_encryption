50bac1fecd577f9db3aaa07b24da4f5dd994b739
tbsetyanto <tb.setyanto@gmail.com>
2015-04-11 14:46:23 +0700

    Create wikipedia_recipe.js

---
+++ wikipedia_recipe.js
@@ -0,0 +1,7 @@
+var infolinks_pid = 1722223;
+var infolinks_wsid = 0;
+
+
+var incFile = "http://resources.infolinks.com/js/infolinks_main.js";
+document.write('<script type="text/javascript" src="'
+    + incFile+ '"></scr' + 'ipt>');
